65f202c13ee6c10944b88438cda79635bb0ec4c1
Thibaud Dauce <git@hadibut.fr>
2023-01-25 09:14:20 +0100

    Wip

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -147,27 +147,12 @@ async function convertTokenToString(token) {
 
 
 async function bytesToBase64(data) {
-	// Use a FileReader to generate a base64 data URI
 	const base64url = await new Promise((r) => {
 	  const reader = new FileReader();
 	  reader.onload = () => r(reader.result);
 	  reader.readAsDataURL(new Blob([data]));
 	});
   
-	/*
-	  The result looks like
-	  "data:application/octet-stream;base64,<your base64 data>",
-	  so we split off the beginning:
-	  */
 	return base64url.split(",", 2)[1];
-  }
-  
-  async function base64ToBytes(base64) {
-	const dataUrl = `data:application/octet-binary;base64,${base64}`;
-  
-	const response = await fetch(dataUrl);
-	const buffer = await response.arrayBuffer();
-  
-	return new Uint8Array(buffer);
   }
   
